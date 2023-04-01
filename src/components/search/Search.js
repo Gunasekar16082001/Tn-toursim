3d19828fbb562b13a829fa88baa320e74138ec08
Vignesh Kumar R <rgunasekar16082001@gmail.com>
2023-04-01 12:31:48 +0530

    second

---
+++ src/components/search/Search.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './SearchStyles.css'
+
+
+function Search() {
+    return (
+        <div name='book' className='search'>
+            <div className="container">
+                <div className="left">
+                    <h2>Welcome to Tamil Nadu!</h2>
+                    <p>Tamil Nadu is very famous for its timeless temples, hill stations, wildlife sanctuaries and gorgeous coastlines. Moreover, the cities in Tamil Nadu have their own charm. Shopping, South cuisines, fairs and festivals in Tamil Nadu are of great importance and equally exciting to explore.It has cerulean mountains, silver waterfalls, verdant vegetation, sandy beaches, mammoth monuments, timeless temples, fabulous wildlife, scintillating sculptures and reverberating rural life.</p>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search
